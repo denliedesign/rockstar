f00e6366d956efc9d83c5f2f4bdcb0facb366d70
denliedesign <customdenlie@gmail.com>
2022-08-30 19:21:10 -0500

    remove class desc

--- resources/views/classes.blade.php
+++ resources/views/classes.blade.php
@@ -10,58 +10,58 @@
         <div class="d-flex justify-content-center my-3">
             <img src="/images/season-13-class-schedule.jpg" alt="" class="img-fluid">
         </div>
-        @can('update', \App\Lesson::class)
-            <p class="text-center"><a href="/lessons/create">Create New Lesson</a></p>
-        @endcan
+{{--        @can('update', \App\Lesson::class)--}}
+{{--            <p class="text-center"><a href="/lessons/create">Create New Lesson</a></p>--}}
+{{--        @endcan--}}
 
-        <div class="separate-new-day">
-            <h2 class="font-weight-bold">monday</h2>
-            @foreach($lessons as $lesson)
-                @if($lesson->day == "monday")
-                   @include('lesson-info')
-                @endif
-            @endforeach
-        </div>
-        <div class="separate-new-day">
-            <h2 class="font-weight-bold">tuesday</h2>
-            @foreach($lessons as $lesson)
-                @if($lesson->day == "tuesday")
-                   @include('lesson-info')
-                @endif
-            @endforeach
-        </div>
-        <div class="separate-new-day">
-            <h2 class="font-weight-bold">wednesday</h2>
-            @foreach($lessons as $lesson)
-                @if($lesson->day == "wednesday")
-                   @include('lesson-info')
-                @endif
-            @endforeach
-        </div>
-        <div class="separate-new-day">
-            <h2 class="font-weight-bold">thursday</h2>
-            @foreach($lessons as $lesson)
-                @if($lesson->day == "thursday")
-                   @include('lesson-info')
-                @endif
-            @endforeach
-        </div>
-        <div class="separate-new-day">
-            <h2 class="font-weight-bold">friday</h2>
-            @foreach($lessons as $lesson)
-                @if($lesson->day == "friday")
-                   @include('lesson-info')
-                @endif
-            @endforeach
-        </div>
-        <div class="separate-new-day">
-            <h2 class="font-weight-bold">saturday</h2>
-            @foreach($lessons as $lesson)
-                @if($lesson->day == "saturday")
-                   @include('lesson-info')
-                @endif
-            @endforeach
-        </div>
+{{--        <div class="separate-new-day">--}}
+{{--            <h2 class="font-weight-bold">monday</h2>--}}
+{{--            @foreach($lessons as $lesson)--}}
+{{--                @if($lesson->day == "monday")--}}
+{{--                   @include('lesson-info')--}}
+{{--                @endif--}}
+{{--            @endforeach--}}
+{{--        </div>--}}
+{{--        <div class="separate-new-day">--}}
+{{--            <h2 class="font-weight-bold">tuesday</h2>--}}
+{{--            @foreach($lessons as $lesson)--}}
+{{--                @if($lesson->day == "tuesday")--}}
+{{--                   @include('lesson-info')--}}
+{{--                @endif--}}
+{{--            @endforeach--}}
+{{--        </div>--}}
+{{--        <div class="separate-new-day">--}}
+{{--            <h2 class="font-weight-bold">wednesday</h2>--}}
+{{--            @foreach($lessons as $lesson)--}}
+{{--                @if($lesson->day == "wednesday")--}}
+{{--                   @include('lesson-info')--}}
+{{--                @endif--}}
+{{--            @endforeach--}}
+{{--        </div>--}}
+{{--        <div class="separate-new-day">--}}
+{{--            <h2 class="font-weight-bold">thursday</h2>--}}
+{{--            @foreach($lessons as $lesson)--}}
+{{--                @if($lesson->day == "thursday")--}}
+{{--                   @include('lesson-info')--}}
+{{--                @endif--}}
+{{--            @endforeach--}}
+{{--        </div>--}}
+{{--        <div class="separate-new-day">--}}
+{{--            <h2 class="font-weight-bold">friday</h2>--}}
+{{--            @foreach($lessons as $lesson)--}}
+{{--                @if($lesson->day == "friday")--}}
+{{--                   @include('lesson-info')--}}
+{{--                @endif--}}
+{{--            @endforeach--}}
+{{--        </div>--}}
+{{--        <div class="separate-new-day">--}}
+{{--            <h2 class="font-weight-bold">saturday</h2>--}}
+{{--            @foreach($lessons as $lesson)--}}
+{{--                @if($lesson->day == "saturday")--}}
+{{--                   @include('lesson-info')--}}
+{{--                @endif--}}
+{{--            @endforeach--}}
+{{--        </div>--}}
 
 
     </div>
